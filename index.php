107f74110d70833c06d5960a61bd958cc493ad2e
22Bi13371 <QuanND.22BI13371@usth.edu.vn>
2024-02-24 01:08:19 +0700

    update index.php

--- index.php
+++ index.php
@@ -6,18 +6,15 @@
     </head>
     <body>
         <h1>Blog title</h1>
-        <p>This paragraph summarises what the blog is about.</p>
-        <h2>Article 1 title</h2>
-        <div>dd Mon YYYY</div>
-        <p>A paragraph summarising article 1.</p>
-        <p>
-            <a href="#">Read more...</a>
-        </p>
-        <h2>Article 2 title</h2>
-        <div>dd Mon YYYY</div>
-        <p>A paragraph summarising article 2.</p>
-        <p>
-            <a href="#">Read more...</a>
-        </p>
+        <p>This blog is about random things that i find interesting and things that i am learnings.</p>
+        
+        <?php for ($postId = 1; $postId <= 3; $postId++): ?>
+            <h2>Article <?php echo $postId ?> title</h2>
+            <div>dd Mon YYYY</div>
+            <p>A paragraph summarising article <?php echo $postId ?>.</p>
+            <p>
+                <a href="#">Read more...</a>
+            </p>
+        <?php endfor ?>
     </body>
 </html>
