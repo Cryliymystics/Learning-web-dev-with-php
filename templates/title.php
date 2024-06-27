5152300a7ccfc2d773607d505adc08f07e6c5653
22Bi13371 <damquan234@gmail.com>
2024-06-28 01:06:38 +0700

    added login system

--- templates/title.php
+++ templates/title.php
@@ -1,4 +1,13 @@
+<div style="float: right;">
+    <?php if (isLoggedIn()): ?>
+    Hello <?php echo htmlEscape(getAuthUser()) ?>.
+        <a href="logout.php">Log out</a>
+    <?php else: ?>
+        <a href="login.php">Log in</a>
+    <?php endif ?>
+</div>
+
 <a href="index.php">
     <h1>Blog title</h1>
 </a>
-<p>This blog is about random things that i find interesting and things that i am learnings.</p>
+<p>This paragraph summarises what the blog is about.</p>
